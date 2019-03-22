c04a0c23a8af676a85e34331ff5a0a90cf9481d9
dorsaf <dorsaf.daaboub24@gmail.com>
2019-03-22 18:18:04 +0300

    profile

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
 module.exports = {
-    mongoURI:'mongodb+srv://dorsaf:dorsaf@cluster0-nupox.mongodb.net/test?retryWrites=true'
+    mongoURI:'mongodb+srv://dorsaf:dorsaf@cluster0-nupox.mongodb.net/test?retryWrites=true',
+    secretOrkey: 'secret'
+
+
 };
